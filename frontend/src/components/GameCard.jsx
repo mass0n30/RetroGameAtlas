0edc4a525a9a907b188cfb249ea79ecf920b8d89
massoncorlette <massoncorlette07@gmail.com>
2025-11-28 13:27:43 -0500

    style: added loading cards for more seamless loading for game cards

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -1,12 +1,22 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles/components/card.module.css';
-
+import { Loader } from 'lucide-react';
 
 function GameCard(props) {
 
+   const [loading, setLoading] = useState(true);
    // eslint-disable-next-line react/prop-types
    const {gameId, coverUrl, index, mount} = props;
+
+  //spinner upon mount with delay
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer); 
+  } ,[loading, setLoading]);
 
    // maybe replace <button> with React Button for more custom options?
    const navigate = useNavigate();
@@ -17,12 +27,19 @@ function GameCard(props) {
 
    return (
     <>
+    { loading ? (
+      <div className={styles.card_loader_container} style={mount ? { animationDelay: `${index * 0.07}s` }: {}}>
+        <div className={styles.card_loader} width="264" height="374">
+            <Loader size={48} className="spinner"color={'#1a2b29ad'}  />
+         </div>
+        </div>
+    ) : (
       <button onClick={(() => handleNavigate())} className={styles.card_button}>
          <div className={styles.card} style={mount ? { animationDelay: `${index * 0.07}s` }: {}} >
             <img className={styles.card_image} src={coverUrl} width="264" height="374"></img>
          </div>
       </button>
-
+    )}
     </>
    );
 }
--- frontend/src/components/GameCardSS.jsx
+++ frontend/src/components/GameCardSS.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles/components/card.module.css';
-
+import {Loader } from 'lucide-react';
 
 function GameCardSS(props) {
 
+    const [loading, setLoading] = useState(true);
    // eslint-disable-next-line react/prop-types
    const {gameId, randomScreenshot, length, index, mount} = props;
 
- 
+   useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer); 
+  } ,[loading, setLoading]);
    // maybe replace <button> with React Button for more custom options?
    const navigate = useNavigate();
 
@@ -18,7 +25,14 @@ function GameCardSS(props) {
 
    return (
     <>
-    { length && length != 0 ? (
+    { loading ? (
+      <div className={styles.cardss_loader_container} style={mount ? { animationDelay: `${index * 0.24}s` }: {}}>
+        <div className={styles.cardss_loader} width="400" height="267">
+            <Loader size={48} className="spinner"color={'#1a2b29ad'}  />
+         </div>
+      </div>
+    ) : (
+    length && length != 0 ? (
       <button onClick={() => handleNavigate()} className={styles.card_button} style={mount ? { animationDelay: `${index * 0.24}s` }: {}}>
         <div className={styles.ss_card} style={mount ? { animationDelay: `${index * 0.24}s` }: {}}>
           <img
@@ -32,10 +46,8 @@ function GameCardSS(props) {
       </button>
     ) : (
       <></>
-    )}
-
-    </>
-   );
-}
-
+    ))
+  }   
+  </>
+  )};
 export default GameCardSS;
