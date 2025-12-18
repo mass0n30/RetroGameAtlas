d535a4f56bbd8134bbad815e6f0f8c1bef3c273f
massoncorlette <massoncorlette07@gmail.com>
2025-12-18 00:11:39 -0500

    adding cors

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(cors());
+app.use(cors({origin:'https://retro-game-atlas.vercel.app/'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
